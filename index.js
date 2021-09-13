dcff1a4c10c8265664b6699b29183b97efc3428d
PedroKassar <pedrokassar8@gmail.com>
2021-09-13 18:56:14 -0300

    Pre-entrega-proyecto

--- index.js
+++ index.js
@@ -7,30 +7,47 @@ function Tienda(name, street, heading) {
 const tienda = new Tienda("Erevan", "Av. Cabildo 1402", "Negocio de calzados");
 console.log(tienda);
 
+
+
+// Catalogo
+
 class Productos {
-    constructor(id, nameP, price) {
+    constructor(id, nameP, price, image) {
         this.id = parseInt(id);
         this.nombre = nameP;
         this.precio = parseFloat(price);
+        this.imagen = image;
     }
 }
 
 const productos = [];
 
-productos.push(new Productos(1, "Zapatillas", 700));
-productos.push(new Productos(2, "Sandalias", 400));
-productos.push(new Productos(3, "Zapatos", 1000));
-productos.push(new Productos(4, "Ojotas", 300));
+productos.push(new Productos(1, "Chinela negra", 700, 'img/foto3.jpg'));
+productos.push(new Productos(2, "Zapato Marcel", 400, 'img/foto4.jpg'));
+productos.push(new Productos(3, "Zapatilla Jaguar", 1000, 'img/foto2.jpg'));
+productos.push(new Productos(4, "Crocks SeaWalk", 300, 'img/foto1.jpg'));
+productos.push(new Productos(5, "Sandalia con taco", 700, 'img/foto5.jpg'));
+productos.push(new Productos(6, "Zapatilla con plataforma", 400, 'img/foto6.jpg'));
+productos.push(new Productos(7, "Chinela puntera cerrada", 1000, 'img/foto7.jpg'));
+productos.push(new Productos(8, "Pantufla con garras", 300, 'img/foto8.jpg'));
+productos.push(new Productos(9, "Pantufla con corderito", 700, 'img/foto9.jpg'));
+productos.push(new Productos(10, "Borcego de goma", 400, 'img/foto10.jpg'));
+productos.push(new Productos(11, "Sandalia jean", 1000, 'img/foto11.jpg'));
+productos.push(new Productos(12, "Zapatilla Jaguar", 300, 'img/foto12.jpg'));
 
 console.log(productos)
 
 for (const producto of productos) {
-    $("#catalogoCarrito").append(`<div><h3>$${producto.precio}</h3>
-<button id=${producto.id} class="botonCarrito">+</button></div>`);
+    $("#catalogoCarrito").append(`<div class="cartaCatalogo">
+    <img src=${producto.imagen}><h2>${producto.nombre}</h2>
+    <h3>$${producto.precio}</h3>
+    <button id=${producto.id} class="botonCarrito">+</button>
+    </div>`);
 }
 
-const botones = document.getElementsByClassName("botonCarrito");
+// 
 
+// Carrito
 const carrito = [];
 
 function AgregarCarrito() {
@@ -51,29 +68,15 @@ function AgregarCarrito() {
 
 }
 
+const botones = document.getElementsByClassName("botonCarrito");
+
 for (const boton of botones) {
     boton.addEventListener("click", AgregarCarrito);
 }
 
-class Cards {
-    constructor(image, nameC) {
-        this.imagen = image;
-        this.nombre = nameC;
-    }
-}
+// 
 
-const cards = [];
-
-cards.push(new Cards(src = 'img/foto3.jpg', "Zapatillas"));
-cards.push(new Cards(src = 'img/foto4.jpg', "Sandalias"));
-cards.push(new Cards(src = 'img/foto2.jpg', "Zapatos"));
-cards.push(new Cards(src = 'img/foto1.jpg', "Ojotas"));
-
-console.log(cards)
-
-for (const card of cards) {
-    $("#catalogoCards").append(`<div><img src=${card.imagen}><h2>${card.nombre}</h2></div>`);
-}
+// Storage
 
 const storage = (Producto, Precio) => {
     localStorage.setItem(Producto, Precio)
@@ -81,49 +84,72 @@ const storage = (Producto, Precio) => {
 
 storage("Lista de Productos", JSON.stringify(productos));
 
-// $(".tituloCatalogo").animate({
-//     padding: '30px',
-// }, 2000);
-
-// $(".tituloCatalogo").animate({
-//     padding: '0px',
-// }, 2000);
-
+// 
 
 // Contacto
 
 function validaForm() {
-    
-if($("#nombreForm").val() == ""){
-    alert("El campo nombre no puede estar vacio");
-    $("#nombreForm").focus();
-    return false;
-}
 
-if($("#mailForm").val() == ""){
-    alert("El campo Email no puede estar vacio");
-    $("#mailForm").focus();
-    return false;
-}
+    if ($("#nombreForm").val() == "") {
+        alert("El campo nombre no puede estar vacio");
+        $("#nombreForm").focus();
+        return false;
+    }
 
-if($("#mensajeForm").val() == ""){
-    alert("El campo Mensaje no puede estar vacio");
-    $("#mensajeForm").focus();
-    return false;
-}
-return true;
+    if ($("#mailForm").val() == "") {
+        alert("El campo Email no puede estar vacio");
+        $("#mailForm").focus();
+        return false;
+    }
+
+    if ($("#mensajeForm").val() == "") {
+        alert("El campo Mensaje no puede estar vacio");
+        $("#mensajeForm").focus();
+        return false;
+    }
+    return true;
 }
 
 const URLGET = "https://jsonplaceholder.typicode.com/posts"
 
-$(document).ready( function() {   
-    $("#botonEnviar").click( function() {     
-        if(validaForm()){                              
-            $.get(URLGET,$("#formdata").serialize(),function(){
-                alert("Mensaje enviado con exito")   
+$(document).ready(function () {
+    $("#botonEnviar").click(function () {
+        if (validaForm()) {
+            $.get(URLGET, function () {
+                alert("Mensaje enviado con exito")
+                $("#formFade").fadeOut(1000);
+                $(`<p>GRACIAS!</P>`).hide().appendTo("#formData").fadeIn(2000);
             });
         }
-    });    
+    });
 });
+
+// 
+
+// Nosotros
+
+$("#nosotros1").append(`<div class="nosotrosParrafo1">
+<p>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+</div>`);
+
+$("#nosotros2").append(`<div class="nosotrosParrafo2">
+<p>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+</div>`);
+
+$("#nosotros3").append(`<div class="nosotrosParrafo3">
+<p>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+</div>`);
+
+$(".nosotrosParrafo1").animate({
+    margin: '0 0 0 50%',
+}, 2000);
+
+$(".nosotrosParrafo2").animate({
+    margin: '0 50% 0 0',
+}, 2000);
+
+$(".nosotrosParrafo3").animate({
+    margin: '0 0 0 50%',
+}, 2000);
 
 // 
